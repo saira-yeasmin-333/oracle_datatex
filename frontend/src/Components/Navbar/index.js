a851b316ac127be461d150b11bce7116934ead52
SanjanaSiraj <sanjanasiraj412@gmail.com>
2022-01-17 12:09:01 +0600

    fixed navbar

---
+++ frontend/src/Components/Navbar/index.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import {
+    Nav,
+    NavLink,
+    Bars,
+    NavMenu,
+    NavBtn,
+    NavBtnLink
+} from './NavbarElements';
+
+const Navbar = () => {
+    return (
+        <>
+            <Nav>
+                <NavLink to='/'>
+                    <h1 className='Banner'>D datatex</h1>
+                </NavLink>
+                <Bars />
+                <NavMenu>
+                    <NavLink to='/about' activeStyle>
+                        About
+                    </NavLink>
+                    <NavLink to='/services' activeStyle>
+                        Services
+                    </NavLink>
+                    <NavLink to='/contact-us' activeStyle>
+                        Contact Us
+                    </NavLink>
+                    <NavLink to='/sign-up' activeStyle>
+                        Sign Up
+                    </NavLink>
+                    {/* Second Nav */}
+                    {/* <NavBtnLink to='/sign-in'>Sign In</NavBtnLink> */}
+                </NavMenu>
+                <NavBtn>
+                    <NavBtnLink to='/sign-in'>Sign In</NavBtnLink>
+                </NavBtn>
+            </Nav>
+        </>
+    );
+};
+
+export default Navbar;
